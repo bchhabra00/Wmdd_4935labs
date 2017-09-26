c7d047a1d7aff8debd2b7720a1543e49b9a09ce1
bchhabra00 <bharatchhabra30@gmail.com>
2017-09-26 20:16:58 +0000

    lab of filtered ls

---
+++ lyn_filteredls/index.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var path = require("path");
+var extension = process.argv[3];
+var dirname = process.argv[2];
+
+fs.readdir(dirname, function(error, data){
+    
+    if(error) return error;
+    
+    var lenghth = data.length;
+    
+    for(var i =0; i< lenghth;i++){
+        
+        var y = data[i];
+        var z= path.extname(y);
+        
+        if(z == "." + extension)
+        
+        console.log(y);
+            
+        
+        
+    }
+    
+    
+});
